9a7597dc201008adf38b8288007f4c020862de2e
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2024-10-10 17:28:31 +0200

    Setup code

---
+++ nokami_store/src/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { Product } from "./pages/Product/Product";
+import { Products } from "./pages/Products/Products";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Footer } from "./components/Footer/Footer";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
