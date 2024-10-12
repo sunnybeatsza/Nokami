35554c5896a48fefda93b62ee456487bc95bcb19
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2024-10-12 20:27:49 +0200

    Changes

--- nokami_store/src/pages/Home/Home.jsx
+++ nokami_store/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.scss";
 import { Slider } from "../../components/Slider/Slider";
 import { FeaturedProducts } from "../../components/FeaturedProducts/FeaturedProducts";
+import { Categories } from "../../components/Catergories/Categories";
 
 export const Home = () => {
   return (
     <div className="home">
       <Slider />
       <FeaturedProducts type="featured" />
+      <Categories />
       <FeaturedProducts type="trending" />
     </div>
   );
