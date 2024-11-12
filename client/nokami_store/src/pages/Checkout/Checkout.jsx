258f4f693f3aca0d38c7b19dc0d5a58bd64a1fc9
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2024-11-12 20:01:19 +0200

    changed backend url

--- client/nokami_store/src/pages/Checkout/Checkout.jsx
+++ client/nokami_store/src/pages/Checkout/Checkout.jsx
@@ -10,7 +10,7 @@ export const Checkout = () => {
     const formData = new FormData(e.target);
 
     try {
-      const response = await fetch("http://localhost:3000/checkout", {
+      const response = await fetch("https://nokami.onrender.com", {
         method: "POST",
         body: formData,
       });
